eee33b6fda83b20582b43d4d7fb0901220d7c500
Márton Páll <s2n5o1@INF.ELTE.HU>
2024-03-19 16:24:33 +0100

    Improve persistence interfaces

---
+++ RobotokModel/Persistence/DataTypes.cs
@@ -0,0 +1,33 @@
+﻿using RobotokModel.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotokModel.Persistence
+{
+    public struct OperationError
+    {
+        public int robotId1;
+        public int robotId2;
+        public OperationErrorType errorType;
+    }
+
+    public enum OperationErrorType
+    {
+        timeout, wallhit, collision
+    }
+
+    public enum TaskEventType
+    {
+        assigned, finished
+    }
+
+    public struct TaskEvent
+    {
+        public int taskId;
+        public int robotId;
+        public TaskEventType eventType;
+    }
+}
--- RobotokModel/Persistence/Interfaces/ILogger.cs
+++ RobotokModel/Persistence/Interfaces/ILogger.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RobotokModel.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +7,8 @@ using System.Threading.Tasks;
 
 namespace RobotokModel.Persistence.Interfaces
 {
+    //TODO: TASK LOGGING IS AMBIGOUS: [TASKID, FROM<-???, TO]
+
     /// <summary>
     /// Creates Log and saves.
     /// <para/>
@@ -21,6 +24,27 @@ namespace RobotokModel.Persistence.Interfaces
         /// <returns></returns>
         Task SaveLog(string path); //alternative: void SaveLog(string path);
 
-        
+        /// <summary>
+        /// Logs a simulation step. Called by the Executor
+        /// </summary>
+        /// <param name="controllerOperations">Operations given by the Controller</param>
+        /// <param name="robotOperations">Operations executed by the Executor</param>
+        /// <param name="errors">Operation errors occurred during execution</param>
+        void LogStep(
+            RobotOperation[] controllerOperations,
+            RobotOperation[] robotOperations,
+            OperationError[] errors
+            );
+
+        /// <summary>
+        /// Logs when Controller timeout occurs. Called by the Executor
+        /// </summary>
+        void LogTimeout();
+
+        /// <summary>
+        /// Logs when a task is assigned or completed. Called by the Distributor or Executor
+        /// </summary>
+        /// <param name="taskEvent"></param>
+        void LogEvent(TaskEvent taskEvent);
     }
 }
