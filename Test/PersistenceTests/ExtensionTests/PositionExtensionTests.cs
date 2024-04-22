e2fc1ed82ae999ff2c7ffedf9c8a28b366ad9e54
András Kollár <wq2gnv@INF.ELTE.HU>
2024-04-22 21:29:38 +0200

    PositionInDirectionTests

---
+++ Test/PersistenceTests/ExtensionTests/PositionExtensionTests.cs
@@ -0,0 +1,58 @@
+﻿using Persistence.DataTypes;
+using Persistence.Extensions;
+
+namespace TestModel.PersistenceTests.ExtensionTests
+{
+    [TestClass]
+    public class PositionExtensionTests
+    {
+        Position newPosition14;
+        Position newPosition24;
+        Position newPosition15;
+        Position newPosition25;
+        public PositionExtensionTests() 
+        {
+            newPosition14.X = 1;
+            newPosition14.Y = 4;
+
+            newPosition24.X = 2;
+            newPosition24.Y = 4;
+
+            newPosition15.X = 1;
+            newPosition15.Y = 5;
+
+            newPosition25.X = 2;
+            newPosition25.Y = 5;
+        }
+
+        [TestMethod]
+        public void PositionInDirectionTests()
+        {
+            Assert.AreEqual(
+                newPosition15,
+                newPosition14.PositionInDirection(Direction.Down)
+                );
+            Assert.AreEqual(
+                newPosition24,
+                newPosition14.PositionInDirection(Direction.Right)
+                );
+            Assert.AreEqual(
+                newPosition24,
+                newPosition25.PositionInDirection(Direction.Up)
+                );
+            Assert.AreEqual(
+                newPosition14,
+                newPosition24.PositionInDirection(Direction.Left)
+                );
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
